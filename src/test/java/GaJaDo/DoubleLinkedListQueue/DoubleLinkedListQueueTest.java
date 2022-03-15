1e4be1bed5e99e7ecb7d398ae31a413a099fb993
Raul Doblas <rauldofe@uma.es>
2022-03-15 12:11:24 +0100

    Some test of DoubleLinkedListQueue

--- src/test/java/GaJaDo/DoubleLinkedListQueue/DoubleLinkedListQueueTest.java
+++ src/test/java/GaJaDo/DoubleLinkedListQueue/DoubleLinkedListQueueTest.java
@@ -1,5 +1,62 @@
 package GaJaDo.DoubleLinkedListQueue;
 
-public class DoubleLinkedListQueueTest {
+import GaJaDo.DequeNode.DequeNode;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+public class DoubleLinkedListQueueTest<T> {
+    private DoubleLinkedListQueue<T> dobleListaEnlazada;
+
+    @BeforeEach
+    public void setup(){
+        dobleListaEnlazada = new DoubleLinkedListQueue<>();
+    }
+
+    @AfterEach
+    public void reboot(){
+        dobleListaEnlazada = null;
+    }
+
+    @Test
+    public void getListAfterAppendLeftOfOneElement(){
+        int expectedValue = 1;
+        dobleListaEnlazada.appendLeft(new DequeNode('x',null,null));
+        assertEquals(expectedValue,dobleListaEnlazada.size());
+    }
+
+    @Test
+    public void getListAfterAppendOfOneElement(){
+        int expectedValue = 1;
+        dobleListaEnlazada.append(new DequeNode('x',null,null));
+        assertEquals(expectedValue,dobleListaEnlazada.size());
+    }
+
+    @Test
+    public void getAEmptyListAfterDeleteFirst(){
+        int expectedValue = 0;
+        dobleListaEnlazada.append(new DequeNode('x',null,null));
+        dobleListaEnlazada.deleteFirst();
+        assertEquals(expectedValue,dobleListaEnlazada.size());
+    }
+
+    @Test
+    public void getAEmptyListAfterDeleteLast(){
+        int expectedValue = 0;
+        dobleListaEnlazada.append(new DequeNode('x',null,null));
+        dobleListaEnlazada.deleteLast();
+        assertEquals(expectedValue,dobleListaEnlazada.size());
+    }
+
+    @Test
+    public void raiseAnExceptionWhenDeleteFirstAnEmptyList(){
+        assertThrows(RuntimeException.class,() -> dobleListaEnlazada.deleteFirst());
+    }
+
+    @Test
+    public void raiseAnExceptionWhenDeleteLastAnEmptyList(){
+        assertThrows(RuntimeException.class,() -> dobleListaEnlazada.deleteFirst());
+    }
 }
